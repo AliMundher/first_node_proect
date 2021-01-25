5361d3de4ce18b82422609a0f5fb32fac6238516
Ali <mundhersaeedai@gmail.com>
2021-01-25 11:32:36 +0100

    seconde commen test

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Salut there");
+const fetch = require("node-fetch");
+
+function getData(year, countryCode) {
+    fetch(`https://date.nager.at/api/v2/publicholidays/${year}/${countryCode}`)
+        .then((res) => {
+            return res.json();
+        }).then((data) => {
+            console.log(data)
+        });
+}
+getData(2017, "be");
